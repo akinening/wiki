f33b6c8a1beda2a2e5c94974b1b63436e53b3dc8
Akinen <akinening@gmail.com>
2022-11-25 11:44:11 +0000

    初回コミット

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Design Wiki
         </h1>
 
         <p className={styles.description}>
@@ -55,15 +55,8 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+        <a href="https://akinen.com" target="_blank" rel="noopener noreferrer">
+          Akinen.com
         </a>
       </footer>
     </div>
